7865f4a9dad983b8d1528147e39afbec763d6c8d
TobisuchtetXD <tobiasrothmeier1807@gmail.com>
2024-12-12 10:09:03 +0100

    optimize world handling

--- src/main/java/org/macemc/OneBlock/command/subcommands/JoinSubCommand.java
+++ src/main/java/org/macemc/OneBlock/command/subcommands/JoinSubCommand.java
@@ -3,7 +3,6 @@ package org.macemc.OneBlock.command.subcommands;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.macemc.OneBlock.data.PlayerData;
-import org.macemc.OneBlock.world.WorldService;
 
 public final class JoinSubCommand extends OneBlockSubCommand
 {
@@ -17,7 +16,7 @@ public final class JoinSubCommand extends OneBlockSubCommand
 	{
 		if (!isPlayer()) tellError("asdad");
 		Player p = getPlayer();
-		Location location = new Location(WorldService.getInstance().getWorld(), 0, 64,0);
+		Location location = new Location(getPlayer().getWorld(), 0, 64,0);
 		p.teleport(location);
 		PlayerData.init(p);
 		tell("You were sent to OneBlock!");
