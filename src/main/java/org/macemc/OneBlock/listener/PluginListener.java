c667db7d50ccaf98982416457aa0f96361812efa
TobisuchtetXD <tobiasrothmeier1807@gmail.com>
2025-01-26 00:38:36 +0100

    adding server regions

--- src/main/java/org/macemc/OneBlock/listener/PluginListener.java
+++ src/main/java/org/macemc/OneBlock/listener/PluginListener.java
@@ -1,32 +1,34 @@
 package org.macemc.OneBlock.listener;
 
 import com.jeff_media.customblockdata.CustomBlockData;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.world.WorldLoadEvent;
+import org.bukkit.event.server.PluginEnableEvent;
 import org.bukkit.persistence.PersistentDataType;
 import org.macemc.OneBlock.OneBlockPlugin;
 import org.macemc.OneBlock.data.Data;
 import org.macemc.OneBlock.world.WorldGuard.WorldGuardService;
 
-public class WorldListener extends OneBlockListenerGroup {
+public class PluginListener extends OneBlockListenerGroup {
 
 	@EventHandler
-	public void onWorldLoad(WorldLoadEvent e) {
-		if (e.getWorld().getName().equals("world")) {
-			Location location = new Location(e.getWorld(), 0, 64, 0);
-			initSpawn(e.getWorld(), location);
+	public void onPluginEnable(PluginEnableEvent e) {
+		if (e.getPlugin().getName().equals("WorldGuard")) {
+			World world = Bukkit.getWorld("world");
+			Location location = new Location(world, 0, 64, 0);
+			assert world != null;
+			initSpawn(world, location);
 			Data.initLocationSearch(location);
 		}
 	}
 
 	public static void initSpawn(World world, Location location) {
-		if (BlockListener.isOneBlock(world.getBlockAt(location))) return;
-		world.setSpawnLocation(0, 65, 0);
-
 		Block block = world.getBlockAt(location);
+		if (BlockListener.isOneBlock(block)) return;
+		world.setSpawnLocation(0, 65, 0);
 
 		CustomBlockData customBlockData = new CustomBlockData(block, OneBlockPlugin.getInstance());
 		customBlockData.set(BlockListener.isOneBlockKey, PersistentDataType.BOOLEAN, true);
